17c2b18409a5661e213baa642f214b88d5790abd
olikidd <79402877+olikidd@users.noreply.github.com>
2021-02-27 09:21:39 +0000

    Motivation and Obstacles

---
+++ Presentation_Gravity.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # The Gravity Model
+# ## Binary Beasts
+# ### Shanjun Zhu, Patrick Leitloff, Su Mae Lee, Oliver Kidd
+
+# # Motivation
+# ### Literature Review
+# ### Gravity Model Literature
+# * Head, K and Mayer, T. (2014) Gravity Equations: Workhorse, Toolkit, and Cookbook
+#     * Defined a simple gravity model, and then expanded into other, more general models to include bilateral trade
+#     
+# 
+# 
+
+# * Anderson, J and van Wincoop, E. (2001) Gravity with Gravitas: A Solution to the Border Puzzle
+#     * Solution to the border problem, shows that basing empirics on theoretically grounded gravity equation affects estimates of impact of national borders on trade
+
+# * Santos Silva, J and S Tenreyro (2006), “The log of gravity ",The Review of Economics and Statistics
+#     * OLS estimates in log-lin relationships with heteroskedasticity cannot be interpreted as elasticities without bias, so the solution is to use a pseudo-maximum-likelihood (PML) estimation technique, that can also deal with zero values for the dependent variable
+
+# ### Brexit Literature
+# * Dhingra, S. et al (2016) The UK Treasury analysis of 'The long-term economic impact of EU membership and the alternatives'
+#     * Critically examines a UK Treasury analysis of the impact of Brexit in 3 different trade agreement scenarios.
+#     * Argues the treasury has been too cautious in some of their assumptions, and so have understated some of the impacts of Brexit on GDP.
+
+# # Obstacles
+# ## Github
+# * As someone who had not used Github before, at first it was slightly confusing
+# * However, after using it for this project, I can now see why it is very useful in group projects and using it throughout the project has allowed us to get more comfortable with it
